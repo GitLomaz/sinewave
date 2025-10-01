39e84c7b37ffe9c5a12c9b84abb5c84bf6f8bd3a
Ian Lomas <you@example.com>
2025-10-01 13:14:32 -0400

    init

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let config = {
+  type: Phaser.AUTO,
+  width: GAME_WIDTH,
+  height: GAME_HEIGHT,
+  parent: "wrapper",
+  scene: [titleScene, gameScene],
+};
---
+++ js/scenes/gameScene.js
@@ -0,0 +1,17 @@
+let gameScene = new Phaser.Class({
+  Extends: Phaser.Scene,
+  initialize: function gameScene() {
+    Phaser.Scene.call(this, {
+      key: "gameScene",
+    });
+  },
+
+  preload: function () {
+  },
+
+  create: function () {
+  },
+
+  update: function (time) {
+  },
+});
